741139230d471d59f0e85d8292d37eee2e8693e9
ventisummer <49564817+storkization@users.noreply.github.com>
2026-04-08 22:59:32 +0900

    v131: PWA 자동 업데이트 로직 안정화 (clients.claim 순서 수정, visibilitychange 쓰로틀링, 중복 방지)

--- sw.js
+++ sw.js
@@ -12,10 +12,11 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
